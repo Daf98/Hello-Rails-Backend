30487a56f286b42c3818bb0239f48992cda41923
Daf <dafne.azzolina@gmail.com>
2022-10-19 12:39:32 -0300

    Add PR link

--- test/controllers/greeting_controller_test.rb
+++ test/controllers/greeting_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class GreetingControllerTest < ActionDispatch::IntegrationTest
-  test "should get message" do
+  test 'should get message' do
     get greeting_message_url
     assert_response :success
   end
